b785795832974cd60b67605282db75b060fd297c
Aleksandr Nekrasov <n.alexsergeevich@gmail.com>
2023-04-10 17:30:48 +0300

    refactor

--- index.js
+++ index.js
@@ -3,27 +3,39 @@ const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
 const createTimerAnimator = () => {
-  return (inputValue) => {
-    const timer = setInterval(() => {
+  let timerId = null;
+  let lastFrameTime = null;
+  const animate = (inputValue, frameTime) => {
+    if (!lastFrameTime) {
+      lastFrameTime = frameTime;
+    }
+    const elapsedTime = frameTime - lastFrameTime;
+    if (elapsedTime >= 1000) {
       const seconds = Math.trunc(inputValue % 60);
-      const minutes = Math.trunc(inputValue / 60 % 60);
-      const hours = Math.trunc(inputValue / 60 / 60 % 60);
+      const minutes = Math.trunc((inputValue / 60) % 60);
+      const hours = Math.trunc((inputValue / 60 / 60) % 60);
 
-      const renderSeconds = (seconds < 10) ? `0${seconds}` : seconds;
-      const renderMins = (minutes < 10) ? `0${minutes}` : minutes;
-      const renderHours = (hours < 10) ? `0${hours}` : hours;
+      const renderSeconds = seconds < 10 ? `0${seconds}` : seconds;
+      const renderMins = minutes < 10 ? `0${minutes}` : minutes;
+      const renderHours = hours < 10 ? `0${hours}` : hours;
 
       if (inputValue === 0) {
-        clearInterval(timer);
-        inputEl.removeAttribute('disabled')
+        cancelAnimationFrame(timerId);
+        inputEl.removeAttribute('disabled');
+      } else {
+        const strTimer = `${renderHours}:${renderMins}:${renderSeconds}`;
+        timerEl.innerHTML = strTimer;
+
+        --inputValue;
       }
-
-      const strTimer = `${renderHours}:${renderMins}:${renderSeconds}`;
-      timerEl.innerHTML = strTimer;
-
-      --inputValue;
-   }, 1000)}
-}
+      lastFrameTime = frameTime;
+    }
+    timerId = requestAnimationFrame((frameTime) => {
+      animate(inputValue, frameTime);
+    });
+  };
+  return animate;
+};
 
 const animateTimer = createTimerAnimator();
 
@@ -38,10 +50,10 @@ inputEl.addEventListener('input', (e) => {
 buttonEl.addEventListener('click', () => {
   const seconds = Number(inputEl.value);
 
-  if(!seconds) return
+  if (!seconds) return;
 
-  animateTimer(seconds);
+  animateTimer(seconds, performance.now());
 
-  inputEl.setAttribute('disabled', '')
+  inputEl.setAttribute('disabled', '');
   inputEl.value = '';
 });
